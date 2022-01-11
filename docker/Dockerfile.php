67cca09fabbcc959848612d247e162261988f492
Hannes Giesenow <hannes.giesenow@elbformat.de>
2022-01-11 12:31:51 +0100

    Add codecov integration

--- docker/Dockerfile.php
+++ docker/Dockerfile.php
@@ -1,5 +1,5 @@
 FROM ezsystems/php:7.4
 RUN apt update &&\
-    apt install -y --no-install-recommends openssh-client &&\
+    apt install -y --no-install-recommends openssh-client gpg gpg-agent &&\
     rm -Rf /var/lib/apt/lists/*
 ENV COMPOSER_MEMORY_LIMIT=-1
