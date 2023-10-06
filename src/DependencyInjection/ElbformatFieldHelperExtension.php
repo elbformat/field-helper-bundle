9d2b7eff319c7c66fbbf05893a8cf39812ccde68
Hannes Giesenow <hannes.giesenow@elbformat.de>
2023-10-06 09:04:13 +0200

    Fixed tests

--- src/DependencyInjection/ElbformatFieldHelperExtension.php
+++ src/DependencyInjection/ElbformatFieldHelperExtension.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Elbformat\FieldHelperBundle\DependencyInjection;
 
+use Elbformat\FieldHelperBundle\FieldHelper\NetgenTagsFieldHelper;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
@@ -18,5 +20,14 @@ class ElbformatFieldHelperExtension extends Extension
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
         $loader->load('services.yml');
+
+        // Add tags only, if bundle is installed
+        if (class_exists('Netgen\TagsBundle\API\Repository\TagsService')) {
+            $fhTags = new Definition(NetgenTagsFieldHelper::class);
+            $fhTags->addTag('elbformat_field_helper.field_helper');
+            $fhTags->setArgument('$tagsService','@eztags.api.service.tags');
+            $container->setDefinition('elbformat_field_helper.field_helper.netgentags', $fhTags);
+        }
+
     }
 }
